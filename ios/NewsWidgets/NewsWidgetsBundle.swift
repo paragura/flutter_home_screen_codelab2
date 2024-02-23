f84ac2edea85281ff7ab46339adbbb1c63f4fcfa
paragura <paragura5041@gmail.com>
2024-02-23 20:50:41 +0900

    Updated by XCode.

---
+++ ios/NewsWidgets/NewsWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NewsWidgetsBundle.swift
+//  NewsWidgets
+//
+//  Created by Wataru Muro on 2024/02/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NewsWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        NewsWidgets()
+    }
+}
